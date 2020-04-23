82f08405221638633865d2c63b671aa0fdf27004
Fabio Pereira <fgpereira@gmail.com>
2020-04-23 13:20:18 -0300

    8.3-mapeamento-da-entidade-cerveja-v1

--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -1,7 +1,38 @@
 package com.algaworks.brewer.model;
 
-public class Estilo {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+
+@Entity(name="estilo")
+public class Estilo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
 	private String nome;
+	
+	@OneToMany (mappedBy = "estilo")
+	private List<Cerveja> cervejas;
+	
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
 
 	public String getNome() {
 		return nome;
@@ -9,5 +40,30 @@ public class Estilo {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
 	}
 }
