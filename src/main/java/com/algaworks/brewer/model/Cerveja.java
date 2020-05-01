119de1f0c8a8d0216669b8cb17df0cf19be989bd
Fabio Pereira <fgpereira@gmail.com>
2020-05-01 19:40:15 -0300

    11.5-callbacks-jpa-salvando-sku-com-caixa-alta-v1

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
@@ -78,6 +80,10 @@ public class Cerveja {
 	private Estilo estilo;
 	
 	
+	@PrePersist @PreUpdate
+	private void prePersistUpdate() {
+		this.sku =  sku.toUpperCase();
+	}
 
 	public Long getCodigo() {
 		return codigo;
