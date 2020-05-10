d32338a51f080824c3771330e85f828a0f28570c
Fábio Gomes Pereira <fpereira@GFA.local>
2020-05-10 18:07:02 -0300

    14.6-criando-pastas-para-salvar-fotos-v1

--- src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -6,27 +6,24 @@ import org.springframework.web.multipart.MultipartFile;
 import com.algaworks.brewer.dto.FotoDTO;
 
 public class FotoStorageRunnable implements Runnable {
-	
+
 	private MultipartFile[] files;
 	private DeferredResult<FotoDTO> resultado;
-	
+	private FotoStorage fotoStorage;
 
-
-	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado, FotoStorage fotoStorage) {
 		this.files = files;
 		this.resultado = resultado;
+		this.fotoStorage = fotoStorage;
 	}
-
-
 
 	@Override
 	public void run() {
-		System.out.println("files: "+ files[0].getSize());
-		//TODO: salvar a foto  no sistema de arquivos - parte demorada
+		this.fotoStorage.salvarTemporariamente(files);
+
 		String nomeFoto = files[0].getOriginalFilename();
 		String contentType = files[0].getContentType();
-		
-		
+
 		resultado.setResult(new FotoDTO(nomeFoto, contentType));
 	}
 
